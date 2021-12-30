54784f5378c6ed954c1925f24e66d711415168c4
sdoshi983 <sdoshi983@rku.ac.in>
2021-12-30 18:52:09 +0530

    menu-item's resuable component finished

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-export const MenuItem = () => {
-    
-}
+export const MenuItem = ({title}) => (
+    <div className='menu-item'>
+        <div className='content'>
+            <h1 className='title'>{title}</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+)
+
+export default MenuItem;
